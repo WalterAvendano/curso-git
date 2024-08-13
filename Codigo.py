dcc2d47d62ffcd146491c870b6cd77893ab4ca9b
Waliva <80907092+WalterAvendano@users.noreply.github.com>
2024-08-13 12:22:03 -0400

    Cargando archivo de Phyton para ejercicio

---
+++ Codigo.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+class CConexion:
+
+    def ConexionBaseDeDatos():
+        try:
+            conexion = mysql.connector.connect(user='root', password='Walugeso01+',
+                                               host='127.0.0.1', database= 'clientesdb',
+                                               port='3306')
+            print("Conexion realizada")
+            return conexion
+        except mysql.connector.Error as error:
+            print("Error al conectarse a la Base de Datos {}".format(error))
+            return conexion
+    ConexionBaseDeDatos()
